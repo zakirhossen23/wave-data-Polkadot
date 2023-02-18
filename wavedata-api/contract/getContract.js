ae3ddd7fee3c337a5a7388d4d3cbaa44169932ac
vybz.live <73053667+zakirhossen23@users.noreply.github.com>
2023-02-19 02:00:16 +0600

    renew

--- wavedata-api/contract/getContract.js
+++ wavedata-api/contract/getContract.js
@@ -2,7 +2,7 @@ import { Abi, ContractPromise } from '@polkadot/api-contract'
 import abiData from './ink_contracts//metadata.json';
 
 
-const address = 'Z7HMk7RPVDvGAZocFj1vcGUv3F2ZMooCb6QXraKPWo3mWcz'//smart contract deployed address 
+const address = 'XqFch64X3bSjH2MdwSnX2pHsdMVDYRLPKqvNfjxuxhErRXF'//smart contract deployed address 
 	
 export default async function getContract(api) {
 
